a503f8aa393cae343a1c6ab18869cf5d175472a8
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2016-07-11 18:51:34 +0200

    Removed commands from run.sh that have to be in DockerFile

--- 5.5/run.sh
+++ 5.5/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-mkdir -p /home/www-data
-chown www-data: /home/www-data
-usermod -d /home/www-data -u $FPM_UID www-data
+usermod -u $FPM_UID www-data
 groupmod -g $FPM_GID www-data
 chown www-data: /var/log/php
 exec /usr/sbin/php5-fpm --allow-to-run-as-root -c /etc/php5/fpm --nodaemonize
--- 7.0/run.sh
+++ 7.0/run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-mkdir -p /home/www-data
-chown www-data: /home/www-data
-usermod -d /home/www-data -u $FPM_UID www-data
+usermod -u $FPM_UID www-data
 groupmod -g $FPM_GID www-data
 chown www-data: /var/log/php
-
-mkdir /run/php
-chown www-data: /run/php
-
 exec /usr/sbin/php-fpm7.0 --allow-to-run-as-root -c /etc/php7.0/fpm --nodaemonize
