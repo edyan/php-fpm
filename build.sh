7b550ab15a1305b2275110dc7954085af242bd56
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2016-08-03 11:02:05 +0200

    Updated all 5.x to have xhprof + mongo enabled and workign

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ if [ -z "$1" -o ! -d "$1" ]; then
     exit 1
 fi
 cd $1
-docker build -t "inetprocesstestphp" .
+docker build -t "inet_php_test" .
 echo ""
 echo ""
 if [ $? -eq 0 ]; then
-    # docker run -v example.cnf:/etc/php5/fpm/user-conf.d/mounted.conf -e "FPM_UID=1000" -e "FPM_GID=1000" inetprocesstestphp
+    # docker run -e "FPM_UID=1000" -e "FPM_GID=1000" --hostname php-ctn --name php inet_php_test
     echo -e "\x1b[1;32mBuild Done\e[0m"
 fi
