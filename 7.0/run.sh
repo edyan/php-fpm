c55c685aae84e947b3c36e420a798b1b35869e8f
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2018-04-03 22:49:17 +0200

    Wrong command to enable / disable commands with php 7.x

--- 7.0/run.sh
+++ 7.0/run.sh
@@ -4,15 +4,15 @@ groupmod -g $FPM_GID www-data
 chown www-data:www-data /var/log/php
 
 # We'll say that we are by default in dev
-php5enmod xdebug
-php5enmod xhprof
+phpenmod xdebug
+phpenmod xhprof
 sed -i 's/^display_errors\s*=.*/display_errors = On/g' /etc/php/7.0//fpm/conf.d/30-custom-php.ini
 sed -i 's/^max_execution_time\s*=.*/max_execution_time = -1/g' /etc/php/7.0/fpm/conf.d/30-custom-php.ini
 
 # If prod has been set ... "clean"
 if [ "$ENVIRONMENT" != "dev" ]; then
-    php5dismod xdebug
-    php5dismod xhprof
+    phpdismod xdebug
+    phpdismod xhprof
     sed -i 's/^display_errors\s*=.*/display_errors = Off/g' /etc/php/7.0/fpm/conf.d/30-custom-php.ini
     sed -i 's/^max_execution_time\s*=.*/max_execution_time = 60/g' /etc/php/7.0/fpm/conf.d/30-custom-php.ini
 fi
