02743410a26797171510dcf24af672198141950c
Emmanuel Dyan <emmanueldyan@gmail.com>
2019-05-11 14:13:16 +0200

    Added cache on images

--- build.sh
+++ build.sh
@@ -23,6 +23,7 @@ cd $1
 
 echo "Building ${TAG}"
 docker build --tag ${TAG} \
+             --cache-from ${TAG} \
              --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
              --build-arg VCS_REF=$(git rev-parse --short HEAD) \
              --build-arg DOCKER_TAG=${TAG} \
