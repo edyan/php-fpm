5d7eae2867326a65e07668a9bffc69044508695a
Emmanuel Dyan <emmanueldyan@gmail.com>
2023-05-11 11:54:04 +0000

    Wrong dgoss export

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,6 +6,11 @@ if [[ -z "${1}" || ! -f "Dockerfile-${1}" ]]; then
     exit 1
 fi
 
+if [[ ! -f /tmp/dgoss ]]; then
+    curl -sL https://raw.githubusercontent.com/goss-org/goss/master/extras/dgoss/dgoss -o /tmp/dgoss
+    chmod +rx /tmp/dgoss
+fi
+
 VERSION=${1}
 DIRECTORY="$( cd "$( dirname "$0" )" && pwd )"
 TAG="${REGISTRY_PREFIX}edyan/php:${VERSION}"
@@ -26,7 +31,7 @@ VERSION_MINOR="$( echo ${VERSION} | sed -e "s/-sqlsrv//g" )"
 LIST_TESTS="TESTS_${VERSION::1}[@]"
 
 # Get goss Path
-export GOSS_PATH=/usr/local/bin/goss
+export GOSS_PATH=/tmp/dgoss
 
 for TESTS in ${!LIST_TESTS}; do
     echo ""
@@ -43,8 +48,8 @@ for TESTS in ${!LIST_TESTS}; do
 
     if [[ ${TESTS} == */few_modules ]]
     then
-        dgoss run -e VERSION=${VERSION_MINOR} -e GOSS_FILES_STRATEGY=cp -e SQLSRV=${SQLSRV} -e "PHP_ENABLED_MODULES=curl xml" ${TAG}
+        /tmp/dgoss run -e VERSION=${VERSION_MINOR} -e GOSS_FILES_STRATEGY=cp -e SQLSRV=${SQLSRV} -e "PHP_ENABLED_MODULES=curl xml" ${TAG}
     else
-        dgoss run -e VERSION=${VERSION_MINOR} -e GOSS_FILES_STRATEGY=cp -e SQLSRV=${SQLSRV} ${TAG}
+        /tmp/dgoss run -e VERSION=${VERSION_MINOR} -e GOSS_FILES_STRATEGY=cp -e SQLSRV=${SQLSRV} ${TAG}
     fi
 done
